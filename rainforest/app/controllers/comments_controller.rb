5e029aca3fbc0dd792da2e926b439aff973fcccb
Eric Stiens <estiens@server.fake>
2013-09-17 18:21:01 -0400

    added user sessions

--- rainforest/app/controllers/comments_controller.rb
+++ rainforest/app/controllers/comments_controller.rb
@@ -1,5 +1,7 @@
 class CommentsController < ApplicationController
-
+  before_filter :ensure_logged_in, :only => [:edit, :create, :show, :update, :destroy]
+  # before_filter :load_product
+  
   def create
   @product=Product.find(params[:product_id])
   @comment=@product.comments.create(comment_params)
--- rainforest/config/routes.rb
+++ rainforest/config/routes.rb
@@ -4,6 +4,8 @@ Rainforest::Application.routes.draw do
   end
 
   resources :users, :except => [:index]
+
+  resources :sessions, :only => [:new, :create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
