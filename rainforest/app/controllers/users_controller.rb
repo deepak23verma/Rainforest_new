3a4db40b049856ebfe13f9040c27d1c207cf7175
Eric Stiens <estiens@server.fake>
2013-09-17 16:30:44 -0400

    user controllers and views

--- rainforest/app/controllers/users_controller.rb
+++ rainforest/app/controllers/users_controller.rb
@@ -4,9 +4,16 @@ class UsersController < ApplicationController
 	end
 
 	def edit
+		@user = User.find(params[:id])
 	end
 
 	def update
+		@user = User.find(params[:id])
+		if @user.update(user_params)
+			redirect_to @user
+		else
+			render :edit
+		end
 	end
 
 	def new
@@ -14,9 +21,18 @@ class UsersController < ApplicationController
 	end
 
 	def create
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to @user
+		else
+			render :new
+		end
 	end
 
 	def destroy
 	end
 
+	def user_params
+		params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+	end
 end
